9e48c0ea3267092e8e7b82a3a0ebf46ef3557dbd
Ankita Masand <amasand23@gmail.com>
2018-12-31 20:53:33 +0530

    Boilerplate

---
+++ boilerplate/webpack.config.js
@@ -0,0 +1,73 @@
+let path = require('path')
+let webpack = require('webpack')
+const HTMLWebpackPlugin = require("html-webpack-plugin")
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const { InjectManifest } = require('workbox-webpack-plugin')
+
+let publicPath = process.env.PUBLIC_PATH || '/'
+let dist = process.env.DIST_DIRECTORY || 'dist'
+
+module.exports = {
+    entry: {
+        bundle: './src/app.js'
+    },
+    output: {
+        path: path.resolve(__dirname, dist),
+        filename: 'js/[name].js',
+        publicPath : publicPath
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['es2015', 'react', 'stage-2'],
+                        plugins: ['transform-runtime']
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    use: [
+                        {
+                            loader: "css-loader",
+                            options: { sourceMap: true }
+                        }, {
+                            loader: "postcss-loader",
+                            options: {
+                                sourceMap: true
+                                // plugins: () => [require('autoprefixer')]
+                            }
+                        }, {
+                            loader: "sass-loader",
+                            options: { sourceMap: true }
+                        }
+                    ]
+                })
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        }),
+        new ExtractTextPlugin({
+            filename: 'css/main.css'
+        }),
+        new CopyWebpackPlugin([
+            {
+                from: './src/manifest.json',
+                to: 'manifest.json'
+            }
+        ]),
+        new InjectManifest({
+            swSrc: './src/service-worker.js'
+        })
+    ]
+}
